412034eb2f1f70dc7f22cdd2c7d22a9c28f0d12b
Magoulo <83534780+Magoulo@users.noreply.github.com>
2023-04-05 13:49:32 +0200

    Added MainActivity to repo

--- App/IMS/app/src/main/java/com/example/ims/MainActivity.kt
+++ App/IMS/app/src/main/java/com/example/ims/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.ims
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// This is a comment
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
