6771de063747de76d37f62244b09357bab4bcc5b
Ayham <haay20uj@student.ju.se>
2023-04-11 04:06:56 +0200

    implemented control fragment Merged with Added Mapsfragment and Fragment dependency to the project in 20-create-mapview

--- App/IMS/app/src/main/java/com/example/ims/ControlFragment.kt
+++ App/IMS/app/src/main/java/com/example/ims/ControlFragment.kt
@@ -4,17 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ListView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModel
+import io.github.controlwear.virtual.joystick.android.JoystickView
+
 
 class ControlFragment : Fragment() {
 
     private val viewModel: ControlFragmentViewModel by activityViewModels()
 
+    private var mTextViewAngle: TextView? = null
+    private var mTextViewStrength: TextView? = null
+    private var mTextViewCoordinate: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -34,7 +37,20 @@ class ControlFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        mTextViewAngle = view.findViewById(R.id.textView_angle) as TextView
+        mTextViewStrength = view.findViewById(R.id.textView_strength) as TextView
+        mTextViewCoordinate = view.findViewById(R.id.textView_coordinate)
 
+        val joystick = view.findViewById(R.id.joystickView) as JoystickView
+        joystick.setOnMoveListener { angle, strength ->
+            mTextViewAngle!!.text = "$angle°"
+            mTextViewStrength!!.text = "$strength%"
+            mTextViewCoordinate!!.text = String.format(
+                "x%03d:y%03d",
+                joystick.normalizedX,
+                joystick.normalizedY
+            )
+        }
 
     }
 
