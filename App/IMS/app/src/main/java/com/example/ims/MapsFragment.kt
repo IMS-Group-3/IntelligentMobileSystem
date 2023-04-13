cb8622cfecb1c26febe224f46e3403eac521da6e
Magoulo <83534780+Magoulo@users.noreply.github.com>
2023-04-13 12:23:38 +0200

    The mower_area_dialogbox.xml is now displayed ontop of the mapFragment.

--- App/IMS/app/src/main/java/com/example/ims/MapsFragment.kt
+++ App/IMS/app/src/main/java/com/example/ims/MapsFragment.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -22,4 +24,31 @@ class MapsFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_maps, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val dialogBuilder = AlertDialog.Builder(requireContext())
+
+        val dialogView = layoutInflater.inflate(R.layout.mower_area_dialogbox, null)
+        dialogBuilder.setView(dialogView)
+
+        // Dialogbox for the mower area input
+        dialogBuilder.setPositiveButton("Submit") { dialog, which ->
+            val editTextWidth = dialogView.findViewById<EditText>(R.id.editTextWidth)
+            val editTextHeight = dialogView.findViewById<EditText>(R.id.editTextHeight)
+            val width = editTextWidth.text.toString().toInt()
+            val height = editTextHeight.text.toString().toInt()
+
+            dialog.dismiss()
+        }
+        dialogBuilder.setNegativeButton("Cancel") { dialog, which ->
+            dialog.dismiss()
+        }
+
+        val dialog = dialogBuilder.create()
+        dialog.show()
+    }
 }
+
+
