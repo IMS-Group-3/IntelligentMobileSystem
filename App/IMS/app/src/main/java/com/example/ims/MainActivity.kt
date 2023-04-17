02b408217f13ef28ca5e666502bcc5eb1bcb2d9b
Ayham <haay20uj@student.ju.se>
2023-04-17 14:01:03 +0200

    Added dependencies, created AppModule as a singleton and added dialog for enabling Bluetooth

--- App/IMS/app/src/main/java/com/example/ims/MainActivity.kt
+++ App/IMS/app/src/main/java/com/example/ims/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.ims
 
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.example.ims.databinding.ActivityMainBinding
-
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
+    @Inject lateinit var bluetoothAdapter: BluetoothAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -36,4 +43,23 @@ class MainActivity : AppCompatActivity() {
         fragmentTransaction.replace(R.id.nav_fragment, fragment)
         fragmentTransaction.commit()
     }
+
+    override fun onStart() {
+        super.onStart()
+        showBluetoothDialog()
+    }
+
+    private fun showBluetoothDialog(){
+        if(!bluetoothAdapter.isEnabled){
+                val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                startBluetoothIntentForResult.launch(enableBluetoothIntent)
+        }
+    }
+
+    private val startBluetoothIntentForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+            if(result.resultCode != Activity.RESULT_OK){
+                showBluetoothDialog()
+            }
+        }
 }
