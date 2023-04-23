eefa0c5022de880897fab43e5b78200d7bb4c998
ingeniator7 <112859997+ingeniator7@users.noreply.github.com>
2023-04-23 14:04:52 +0200

    [Fixed] function name

--- Back-End/web-app/src/db.js
+++ Back-End/web-app/src/db.js
@@ -29,7 +29,7 @@ module.exports = function ({
             )
         },
 
-        fetchImage(callback) {
+        fetchImages(callback) {
             db.all(
                 `select * from image`,
                 function (err, images) {
