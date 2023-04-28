8904fb193ae6ea72f5ed66ff97adea2fba2195a8
Magoulo <83534780+Magoulo@users.noreply.github.com>
2023-04-28 15:49:27 +0200

    Added a function for retrieveing all images

--- App/IMS/app/src/main/java/com/example/ims/services/ImageApi.kt
+++ App/IMS/app/src/main/java/com/example/ims/services/ImageApi.kt
@@ -6,7 +6,7 @@ import java.net.HttpURLConnection
 import java.net.URL
 
 class ImageApi {
-    private val baseUrl = "https://your-backend-endpoint.com/"
+    private val baseUrl = "http://16.16.68.202"
 
     fun getImageById(id: Int, callback: (Result<String>) -> Unit) {
         val url = URL("$baseUrl/image/$id")
@@ -33,4 +33,31 @@ class ImageApi {
             }
         }.start()
     }
+
+    fun getAllImages(callback: (Result<String>) -> Unit) {
+        val url = URL("$baseUrl/image")
+        Thread {
+            try {
+                val connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = "GET"
+                connection.connect()
+
+                val responseCode = connection.responseCode
+
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    val reader = BufferedReader(InputStreamReader(connection.inputStream))
+                    val response = reader.readText()
+                    reader.close()
+                    callback(Result.success(response))
+                } else {
+                    callback(Result.failure(Exception("Error: $responseCode")))
+                }
+
+                connection.disconnect()
+            } catch (e: Exception) {
+                callback(Result.failure(e))
+            }
+        }.start()
+    }
+
 }
