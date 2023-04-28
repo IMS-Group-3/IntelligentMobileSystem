a4ca5d2b679a20700f3aa503329ef0ff359ff958
Magoulo <83534780+Magoulo@users.noreply.github.com>
2023-04-28 16:07:39 +0200

    refactored the code in the ImageApi

--- App/IMS/app/src/main/java/com/example/ims/services/ImageApi.kt
+++ App/IMS/app/src/main/java/com/example/ims/services/ImageApi.kt
@@ -4,12 +4,17 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Base64
+import org.json.JSONObject
+
 
 class ImageApi {
     private val baseUrl = "http://16.16.68.202"
 
-    fun getImageById(id: Int, callback: (Result<String>) -> Unit) {
-        val url = URL("$baseUrl/image/$id")
+    private fun fetchImage(urlString: String, callback: (Result<Bitmap>) -> Unit) {
+        val url = URL(urlString)
         Thread {
             try {
                 val connection = url.openConnection() as HttpURLConnection
@@ -22,7 +27,14 @@ class ImageApi {
                     val reader = BufferedReader(InputStreamReader(connection.inputStream))
                     val response = reader.readText()
                     reader.close()
-                    callback(Result.success(response))
+
+                    // Decode the image data
+                    val imageData = JSONObject(response).getJSONObject("imageData")
+                    val encodedImage = imageData.getString("encodedImage")
+                    val decodedBytes = Base64.decode(encodedImage, Base64.DEFAULT)
+                    val decodedBitmap = BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.size)
+
+                    callback(Result.success(decodedBitmap))
                 } else {
                     callback(Result.failure(Exception("Error: $responseCode")))
                 }
@@ -34,30 +46,12 @@ class ImageApi {
         }.start()
     }
 
-    fun getAllImages(callback: (Result<String>) -> Unit) {
-        val url = URL("$baseUrl/image")
-        Thread {
-            try {
-                val connection = url.openConnection() as HttpURLConnection
-                connection.requestMethod = "GET"
-                connection.connect()
-
-                val responseCode = connection.responseCode
-
-                if (responseCode == HttpURLConnection.HTTP_OK) {
-                    val reader = BufferedReader(InputStreamReader(connection.inputStream))
-                    val response = reader.readText()
-                    reader.close()
-                    callback(Result.success(response))
-                } else {
-                    callback(Result.failure(Exception("Error: $responseCode")))
-                }
-
-                connection.disconnect()
-            } catch (e: Exception) {
-                callback(Result.failure(e))
-            }
-        }.start()
+    fun getImageById(id: Int, callback: (Result<Bitmap>) -> Unit) {
+        fetchImage("$baseUrl/image/$id", callback)
     }
 
+    fun getAllImages(callback: (Result<Bitmap>) -> Unit) {
+        fetchImage("$baseUrl/image", callback)
+    }
 }
+
