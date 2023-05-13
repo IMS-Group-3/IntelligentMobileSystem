07f138df1e54c566bb2b7ebebe011e1bc97153bb
Ayham <haay20uj@student.ju.se>
2023-05-13 09:16:41 +0200

    Cleaned Home fragment

--- App/IMS/app/src/main/java/com/example/ims/HomeFragment.kt
+++ App/IMS/app/src/main/java/com/example/ims/HomeFragment.kt
@@ -1,30 +1,10 @@
 package com.example.ims
 
-import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.activity.viewModels
-import androidx.core.content.ContextCompat
-import androidx.fragment.app.viewModels
-import com.example.ims.data.ConnectionState
-import android.Manifest
-import android.content.pm.PackageManager
-import android.util.Log
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.fragment.app.activityViewModels
-
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -40,19 +20,12 @@ class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private val locationViewModelTemp: LocationViewModelTemp by activityViewModels()
-    var bleConnectionState: ConnectionState? = null
 
-    val x : TextView? = null
-    val y : TextView? = null
-    val collisionAvoidance : TextView? = null
-    val connectionState : TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
-            bleConnectionState = locationViewModelTemp.connectionState.value
         }
     }
 
@@ -67,35 +40,9 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-        bleConnectionState = locationViewModelTemp.connectionState.value
-
-        /*if(allPermissionsGranted() && bleConnectionState == ConnectionState.Uninitialized){
-            locationViewModelTemp.initializeConnection()
-        }*/
-        //Log.i("fdghjklöjkhgfdsdhjgkjhlkjlölyitukyjdghc",bleConnectionState.toString() )
-        val x = view.findViewById<TextView>(R.id.textView_x_coordinate)
-        val y = view.findViewById<TextView>(R.id.textView_y_coordinate)
-        val collisionAvoidance = view.findViewById<TextView>(R.id.textView_collision_avoidance)
-        val connectionState = view.findViewById<TextView>(R.id.textView_connection_state)
-
-
     }
 
-    override fun onStart() {
-        super.onStart()
-        /*if(allPermissionsGranted() && bleConnectionState == ConnectionState.Disconnected){
-            locationViewModelTemp.reconnect()
-        }*/
 
-    }
-
-    override fun onStop() {
-        super.onStop()
-       /* if(allPermissionsGranted() && bleConnectionState == ConnectionState.Connected){
-            locationViewModelTemp.disconnect()
-        }*/
-    }
     companion object {
         /**
          * Use this factory method to create a new instance of
@@ -114,52 +61,5 @@ class HomeFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
-    }
-    private fun allPermissionsGranted():Boolean{
-
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S){
-            val bluetoothScanPermission = ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.BLUETOOTH_SCAN)
-            val bluetoothConnectPermission = ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.BLUETOOTH_CONNECT)
-
-            if (bluetoothScanPermission != PackageManager.PERMISSION_GRANTED){ return false }
-            if (bluetoothConnectPermission != PackageManager.PERMISSION_GRANTED){ return false  }        }
-
-        val accessFineLocationPermission = ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
-        val accessCoarseLocationPermission = ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
-
-        if (accessFineLocationPermission != PackageManager.PERMISSION_GRANTED){ return false  }
-        if (accessCoarseLocationPermission != PackageManager.PERMISSION_GRANTED){ return false  }
-        return true
-    }
-    private fun updateFragment (bleConnectionState: ConnectionState){
-        if(bleConnectionState == ConnectionState.CurrentlyInitializing){
-            x?.text = ""
-            y?.text = ""
-            collisionAvoidance?.text = ""
-            connectionState?.text = ""
-
-            if(locationViewModelTemp.initializingMessage != null){
-                connectionState?.text = locationViewModelTemp.initializingMessage!!
-            }
-
-        }else if(!allPermissionsGranted()){
-            connectionState?.text = "Go to the app setting and allow the missing permissions."
-        }else if(locationViewModelTemp.errorMessage != null){
-
-            connectionState?.text = locationViewModelTemp.errorMessage!!
-
-        }else if(bleConnectionState == ConnectionState.Connected){
-
-            x?.text = "X: ${locationViewModelTemp.x}"
-            y?.text = "Y: ${locationViewModelTemp.y}"
-            collisionAvoidance?.text  = "Collisio nAvoidance: ${locationViewModelTemp.collisionAvoidance}"
-
-        }else if(bleConnectionState == ConnectionState.Disconnected){
-            connectionState?.text = "Initialize again"
-
-        }else {
-            connectionState?.text = "No device was found"
-
-        }
     }
 }
