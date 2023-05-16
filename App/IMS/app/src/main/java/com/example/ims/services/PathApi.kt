7e9e22f5a22b627161169b95fbbfd41b41250ba7
Ayham <haay20uj@student.ju.se>
2023-05-16 22:32:01 +0200

    Added paths fetch function and path data class

--- App/IMS/app/src/main/java/com/example/ims/services/PathApi.kt
+++ App/IMS/app/src/main/java/com/example/ims/services/PathApi.kt
@@ -10,6 +10,8 @@ import org.json.JSONTokener
 
 class PathApi {
     private val baseUrl = "http://16.16.68.202"
+    private val pathesUrl = "http://16.16.68.202"
+
 
     private fun fetchPath(urlString: String, callback: (MutableMap<String, MutableList<String>>) -> Unit) {
         val url = URL(urlString)
@@ -56,5 +58,58 @@ class PathApi {
 
     fun getAllPositions(callback: (MutableMap<String, MutableList<String>>) -> Unit) {
         fetchPath("$baseUrl/paths", callback)
+    }
+    private fun fetchPaths(urlString: String, callback: (JSONArray) -> Unit) {
+        val url = URL(urlString)
+        var map = mutableMapOf<String, MutableList<String>>()
+
+        Thread {
+            try {
+                val connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = "GET"
+                connection.connect()
+                val responseCode = connection.responseCode
+
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    val reader = BufferedReader(InputStreamReader(connection.inputStream))
+                    val response = reader.readText()
+                    reader.close()
+
+                    val jsonArray = JSONTokener(response).nextValue() as JSONArray
+                    for (i in 0 until jsonArray.length()) {
+
+                        val positionId = jsonArray.getJSONObject(i).getString("positionId")
+                        val x = jsonArray.getJSONObject(i).getString("x")
+                        val y = jsonArray.getJSONObject(i).getString("y")
+                        val timestamp = jsonArray.getJSONObject(i).getString("timestamp")
+                        val collisionOccurred = jsonArray.getJSONObject(i).getString("collision_occured")
+
+                        map[positionId] = mutableListOf(x, y, timestamp, collisionOccurred)
+                    }
+                    Log.i("ff",jsonArray.toString())
+                    callback(jsonArray)
+                }
+
+                connection.disconnect()
+            } catch (e: Exception) {
+                e.printStackTrace()
+                callback(JSONArray())
+            }
+        }.start()
+    }
+    fun getAllPaths(id: Int, callback: (ArrayList<String>) -> Unit) {
+        val arrayList: ArrayList<String> = arrayListOf()
+        fetchPaths("http://16.16.68.202/paths/",){
+            for (i in 0 until it.length()) {
+                val path = it.getJSONObject(i)
+                val pathId = path.getString("pathId")
+                val startTime = path.getString("start_time")
+                val endTime = path.getString("end_time")
+
+                arrayList.add("hhj")
+                //map[positionId] = mutableListOf(x, y, timestamp, collisionOccurred)
+            }
+            callback(arrayList)
+        }
     }
 }
