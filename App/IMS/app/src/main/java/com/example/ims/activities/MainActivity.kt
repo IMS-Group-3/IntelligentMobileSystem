3bfed504bb3970e80dadb62e23c35350cf10cc76
Ayham <haay20uj@student.ju.se>
2023-05-20 12:47:12 +0200

    Added navigation from HistoryFragment to PathMapFragment

--- App/IMS/app/src/main/java/com/example/ims/activities/MainActivity.kt
+++ App/IMS/app/src/main/java/com/example/ims/activities/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.activity.viewModels
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.example.ims.*
 import com.example.ims.databinding.ActivityMainBinding
 import com.example.ims.fragments.MapsFragment
@@ -28,6 +29,8 @@ class MainActivity : AppCompatActivity(){
     @Inject lateinit var bluetoothAdapter: BluetoothAdapter
     private val controlViewModel: ControlViewModel by viewModels()
     private val historyViewModel: HistoryViewModel by viewModels()
+    private val pathMapViewModel: PathMapViewModel by viewModels()
+
     val pathApi = PathApi()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -69,7 +72,9 @@ class MainActivity : AppCompatActivity(){
         historyViewModel.run {
             pathInfoFromItemClick.observe(this@MainActivity) {
                 if (it != null){
+                    pathMapViewModel.pathId.value = it.pathId.toInt()
                     replaceFragment(PathMapFragment())
+                    pathMapViewModel.isHistory.value = true
                 }
             }
         }
