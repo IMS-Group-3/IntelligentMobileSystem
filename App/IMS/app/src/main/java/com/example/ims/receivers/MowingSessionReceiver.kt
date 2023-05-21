06325662975ff493cd9b6b432a5288b93bd5e7b5
Magoulo <83534780+Magoulo@users.noreply.github.com>
2023-05-21 16:21:41 +0200

    Added the call for the auitomatic mowing to the MowingSessionReceiver.kt

--- App/IMS/app/src/main/java/com/example/ims/receivers/MowingSessionReceiver.kt
+++ App/IMS/app/src/main/java/com/example/ims/receivers/MowingSessionReceiver.kt
@@ -1,15 +1,18 @@
 package com.example.ims.receivers
 
 import MOWING_SESSION_CHANNEL_ID
+import PathApi
 import android.Manifest
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.example.ims.R
+import com.example.ims.data.Commands
 
 class MowingSessionReceiver : BroadcastReceiver() {
 
@@ -31,6 +34,17 @@ class MowingSessionReceiver : BroadcastReceiver() {
             return
         }
         notificationManager.notify(1, notificationBuilder.build())
+
+        Thread {
+            try {
+                PathApi().sendManualCommand(Commands.M_AUTO){
+                    Log.i("responseCode: ", it.toString())
+                }
+            } catch (e: Exception) {
+                // Handle exceptions
+            }
+        }.start()
+
     }
 }
 
